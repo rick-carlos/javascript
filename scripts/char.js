a06e03288d658cbcd64adb5f26099d37139b0212
rick-kinzey <chcavil@gmail.com>
2024-11-06 21:04:37 -0300

    Movimentando personagem

--- scripts/char.js
+++ scripts/char.js
@@ -1,5 +1,21 @@
-const personagem = document.querySelector('.personagem');
+console.log("arquivo javascript carregando com sucesso");
 
-function moverPersonagem() {
-    
+const personagem = document.getElementById('personagem');
+let posicao = 0;
+
+function moverPersonagem(direction){
+    posicao += direction;
+    personagem.style.left = posicao + 'px';
+
+
 }
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'd' || event.key === 'D'){
+        moverPersonagem(10);
+    }
+    else if (event.key === 'a' || event.key === 'A'){
+        moverPersonagem(-10);
+    }
+});
